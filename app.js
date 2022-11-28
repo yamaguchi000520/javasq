994bf40f3809816ec9ec6eea070af0a6aa85a86d
EC2 Default User <ec2-user@ip-172-31-16-16.ap-northeast-1.compute.internal>
2022-11-28 10:14:00 +0000

    fix,3

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 let hello='Hello World';
-alert(hello);
+// alert(hello);
+let int1=1;
+let int2=-10;
+let float1=3.14
+let str1='javascriptを覚えよう';
+// alert(str1)
+let str2='hallo';
+let str3='world';
+alert(str2+str3);
