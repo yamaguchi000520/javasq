552b25bb0554d591a143f620d2494fe750c05ef1
EC2 Default User <ec2-user@ip-172-31-16-16.ap-northeast-1.compute.internal>
2022-11-29 07:35:06 +0000

    jq start

---
+++ jq_app2.js
@@ -0,0 +1,16 @@
+// $(function(){
+//   $('.box1').css({
+//     'background-color':'#0000ff',
+//     'height':'100px'
+//   })
+// })
+
+
+$(function(){
+  $('.box1').hide();
+
+  // $('.box1').show();
+  // $('.box1').css({'background-color':'#0000ff'});
+
+  // $('.box1').slideUp();
+});
