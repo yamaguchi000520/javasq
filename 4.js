60613101dcb193ead299f9898fa8a54795373174
kei yamaguchi <kei.yamaguchi00520@gmail.com>
2023-02-02 10:54:01 +0000

    rest

---
+++ 4.js
@@ -0,0 +1,13 @@
+// データ型
+// Number(数値)
+// String(文字列)
+// Bootlean(論理型)
+// Null
+// Undefined
+
+// 何型かを調べる typeof 値
+console.log(typeof 10)
+// 10のデータを調べてlogを出す
+
+console.log(typeof 100)//数値として認識
+console.log(typeof '100')//文字列として認識
