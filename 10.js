49ac5467e65314ffcc8f807c1deadd2d7ac12624
kei yamaguchi <kei.yamaguchi00520@gmail.com>
2023-02-10 11:57:59 +0000

    rest

---
+++ 10.js
@@ -0,0 +1,24 @@
+//Javascriptにもともと用意されているオブジェクト
+
+//Math
+//円周率のプロパティ名はPI
+console.log(Math.PI);
+
+//絶対値の計算
+console.log(Math.abs(-10)); //絶対値はabsを使用する
+
+//四捨五入、切り捨て切り上げ
+console.log(Math.round(1.4)); //四捨五入
+console.log(Math.floor(10.3));  //切り捨て
+console.log(Math.ceil(10.3));  //切り上げ
+
+//ランダムの数を生成
+console.log(Math.random());
+
+//サイコロを作ろう
+
+
+
+
+
+
